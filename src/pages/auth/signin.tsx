8e8312733cc7c2b19f2843c392b9390717e34db5
brunoseleghin <brunoseleghin@gmail.com>
2024-03-18 22:04:57 -0300

    added toaster

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,10 +1,45 @@
-import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      if (data.email !== '') {
+        console.log(data)
+
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+        toast.success('Enviado link de autenticação ao e-mail informado!', {
+          action: {
+            label: 'Reenviar',
+            onClick: () => handleSignIn(data),
+          },
+        })
+      } else {
+        throw new Error()
+      }
+    } catch {
+      toast.error('O e-mail informado não é válido!')
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -19,13 +54,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
